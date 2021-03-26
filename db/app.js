35b5c58ce1bf401efba8d12d393415de5ae1b808
Daniel Lucas <danomiterock@yahoo.com>
2021-03-25 18:19:45 -0700

    tutoring session

--- db/app.js
+++ db/app.js
@@ -1,2 +1,2 @@
-const { v4: uuidv4 } = require('uuid');
-uuidv4();
+// const { v4: uuidv4 } = require('uuid');
+// uuidv4();
--- server.js
+++ server.js
@@ -1,38 +1,54 @@
-const { fstat } = require("node:fs");
 const fs = require('fs');
-const uuid = require('uuid/v1');
+const uuid = require('uuid');
 const express = require('express');
 const path = require('path');
-const data = require(__dirname + '/./db/debug.json');
 const app = express();
 const PORT = process.env.PORT || 8000;
+const db = require("./db/db.json")
 
 //Middleware functions
-add.use(express.urlencoded({ extended: true }));
-add.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 //static file hosting for public directory
-applicationCache.use(express.static("public"));
+app.use(express.static("public"));
 
-//HTML routes
-app.get("/notes", function (req, res){
-    res.sendFile(path.join(__dirname, "public", "notes.html"));
-});
 
 //API routes
 app.get("/api/notes", function (req, res){
     //Retrieve all notes and res.json them back to the front end
-    fs.readFile("./db/db.json", "UTF-8", function (err, data){
-        res.json(JSON.parse(data));
+    fs.readFile("db/db.json", "UTF-8", function (err, data){
+        return err ? console.log(err) : res.json(JSON.parse(data));        
     });
 });
 
 app.post("/api/notes", function (req, res){
     //Create a note from req.body
+    //read create save read again
+    console.log(req.body);
+    fs.readFile("db/db.json", "UTF-8", function (err, data){
+        if (err) throw err
+    const jsonData = JSON.parse(data);
+    const newNote = {id:db.length+1, title:req.body.title, text:req.body.text};
+    jsonData.push(newNote);
+    fs.writeFile("db/db.json", JSON.stringify(jsonData), function (err, data){
+        if (err) throw err 
+        res.end(data)
+    });
+    });
 });
 
 app.delete("/api/notes", function (req, res){
     //Delete a note based off id
+    
+});
+
+//HTML routes
+app.get("/notes", function (req, res){
+    res.sendFile(path.join(__dirname, "public", "notes.html"));
+});
+app.get("/", function (req, res){
+    res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.listen(PORT, () => console.log("App listening on port " + PORT));
